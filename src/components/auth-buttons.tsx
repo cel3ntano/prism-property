e93d47bd6dd47f4d43fc89c29d6021b1d15d5001
cel3ntano <adriano2024celentano@gmail.com>
2025-03-08 23:46:30 +0100

    feat: add authentication routes and UI components
    Added login and register routes
    Implemented basic navbar layout in the root layout
    Integrated shadcn-ui into the project
    Added 'Continue with Google' login button
    Created authentication context to manage user session
    Display logged-in user in the navbar

---
+++ src/components/auth-buttons.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Link from 'next/link';
+import { useAuth } from '../../context/auth';
+
+export default function AuthButtons() {
+  const auth = useAuth();
+
+  return (
+    <div>
+      {!!auth?.currentUser && (
+        <>
+          <div>{auth.currentUser.email}</div>
+          <div>Logout</div>
+        </>
+      )}
+      {!auth?.currentUser && (
+        <>
+          <Link href="login">Login</Link>
+          <Link href="register">Signup</Link>
+        </>
+      )}
+    </div>
+  );
+}
